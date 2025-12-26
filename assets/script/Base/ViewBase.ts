359bb49c79b6dd03acb7b9483a3c84338626c110
yuhong <yuhong@shinet.cn>
2025-12-26 16:22:23 +0800

    v1.3.0

--- assets/script/Base/ViewBase.ts
+++ assets/script/Base/ViewBase.ts
@@ -1,28 +1,23 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+import { UIManager } from "../GameManager/UIManager";
+import { CFTools } from "../Tools/CFTools";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class ViewBase extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start() {
-
+    /** UI面板点击关闭按钮的操作，注意节点名需要和脚本名一致 */
+    clickCloseBtn(): void {
+        UIManager.CloseUI(this.node.name);
     }
 
-    // update (dt) {}
+    /** 打开UI时进行的操作，可以初始化数据，也可以设计打开动画，还可以携带参数 */
+    openUI(): void {
+        CFTools.log("test");
+    }
+
+    /** 关闭UI时的操作，默认为直接销毁节点 */
+    closeUI(): void {
+        this.node.destroy();
+    }
 }
---
+++ assets/script/View/OverUI.ts
@@ -0,0 +1,11 @@
+import ViewBase from "../Base/ViewBase";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class OverUI extends ViewBase {
+
+    protected start(): void {
+
+    }
+}
